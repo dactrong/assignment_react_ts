36a7b23356f730cc289e75daba38af3b3d949b66
dactrong <trongndph13723@fpt.edu.vn>
2022-03-26 08:44:02 +0700

    update: diao dien admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/client/HomePage'
 import WebsiteLayout from './pages/client/WebsiteLayout'
 import Products from './pages/client/Products'
+import AdminLayout from './pages/admin/AdminLayout'
+import Dashboard from './pages/admin/Dashboard'
 
 function App() {
 
@@ -15,6 +17,10 @@ function App() {
         <Route path="/" element ={<WebsiteLayout/>}>
             <Route index element={<HomePage/>}/>
             <Route path="/product" element ={<Products/>}/>
+        </Route>
+        <Route path ="admin" element ={<AdminLayout/>}>
+          <Route index element={<Dashboard/>}/>
+
         </Route>
       </Routes>
     </div>
