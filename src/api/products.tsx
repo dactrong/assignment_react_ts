a0cbbb2f9ff9647da291846eb55de4b2a77c66da
dactrong <trongndph13723@fpt.edu.vn>
2022-03-27 20:46:19 +0700

    feact: API

---
+++ src/api/products.tsx
@@ -0,0 +1,22 @@
+import instance from "./instance";
+
+export const create = (product:any) =>{
+       const url = `product`;
+       return  instance.post(url,product) 
+}
+export const list = () =>{
+    const url = `product`;
+    return instance.get(url)
+}
+export const read = (_id:number)=>{
+    const url = `product/${_id}`;
+    return instance.get(url)
+}
+export const remove = (_id:number)=>{
+    const url = `product/${_id}`;
+    return instance.delete(url)
+}
+export const update= (product:any) =>{
+    const url = `product/${product._id}`;
+    return instance.put(url, product)
+}
